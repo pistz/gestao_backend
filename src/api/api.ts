5a1d9f57c8a1f1af96cab11154687755033dd618
fgmarcal <felipegarciamarcal@gmail.com>
2024-03-21 21:38:40 -0300

    install fastify

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,22 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
-import exp from 'constants';
+import cors, { FastifyCorsOptions } from '@fastify/cors'
+import Fastify from 'fastify';
 
 const allowedOrigins = ['http://localhost:5173'];
 
-const options: cors.CorsOptions = {
+const options:FastifyCorsOptions = {
     origin:allowedOrigins
 };
 
-const api = express();
+const api = Fastify();
 
-api.use(cors(options));
+api.register(cors, options);
 
-api.use(express.json);
 
 //TODO- rotas
+api.get("/", async (_request, reply)=>{
+    return reply.send({message:"gestao frequencia"});
+});
 
-api.get("/api/v1", (req:Request, res:Response)=> res.send("API Gestao de Frequencia Online"));
+api.get("/api/v1", (_request, reply)=> reply.send("API Gestao de Frequencia Online"));
 
 export {api};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import {api} from "./api/api";
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
-api.listen(port, () => {
-    console.log(`Gestao de Frequência Online http://localhost:${port}`);
+api.listen({
+    host:'0.0.0.0',
+    port: port,
+}
+).then(()=>{
+    console.log(`Server running on port: ${port}`)
 });
